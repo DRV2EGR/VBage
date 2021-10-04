d79b6b5f21d931e8be2e64ed1e71f4d999ffdc71
DRV2VNA <1664286@gmail.com>
2021-10-04 18:25:18 +0300

    Developed Exceptions

---
+++ src/main/java/ru/vbage/exception/ApiException.java
@@ -0,0 +1,15 @@
+package ru.vbage.exception;
+
+/**
+ * The type Api exception.
+ */
+public abstract class ApiException extends RuntimeException {
+    /**
+     * Instantiates a new Api exception.
+     *
+     * @param message the message
+     */
+    public ApiException(String message) {
+        super(message);
+    }
+}

0b79b838fba8de166e1e3d6177d08d201fd585a8
DRV2VNA <1664286@gmail.com>
2021-10-04 16:48:44 +0300

    Initial commit

---
+++ src/test/java/ru/vbage/VbageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vbage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
